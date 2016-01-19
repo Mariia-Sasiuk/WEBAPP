3a82b80fa41b6756388b320f09b4e42168f6e5e8
Mariia Sasiuk <melody.mos1@gmail.com>
2016-01-20 00:20:26 +0200

    Issue #7 : try-with-resources was used for DataBase class

--- web_mod/src/main/application/lab3/model/util/DataBase.java
+++ web_mod/src/main/application/lab3/model/util/DataBase.java
@@ -8,57 +8,31 @@ import java.sql.*;
 
 public class DataBase {
 
-    private static Connection getConnection() {
-        Connection connection = null;
-        try {
-            InitialContext context = new InitialContext();
-            DataSource dataSource = (DataSource) context.lookup("studentDB");
-            connection = dataSource.getConnection();
-        } catch (NamingException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return connection;
-    }
-
-    public static void closeConnection(){
-        if (getConnection()!=null)
-            try {
-                getConnection().close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-    }
-
         public static void executeSelect(String query, ResultSetHandler handler ){
-            try {
-                Statement statement = getConnection().createStatement();
-                ResultSet rs = statement.executeQuery(query);
+            try(Connection connection = ((DataSource) new InitialContext().lookup("studentDB")).getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery(query))
+            {
                 handler.onResultSet(rs);
-
-                rs.close();
-                statement.close();
-
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
-            finally {
-                closeConnection();
+            catch (SQLException | NamingException e)
+            {
+                e.printStackTrace();
             }
         }
 
         public static void executeInsert(String query, ResultSetHandler handler){
-            try(PreparedStatement prep = getConnection().prepareStatement(query)) {
+            try(Connection connection = ((DataSource) new InitialContext().lookup("studentDB")).getConnection();
+                PreparedStatement prep = connection.prepareStatement(query))
+            {
                 handler.onInsertSet(prep);
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
-            finally {
-                closeConnection();
+            catch (SQLException | NamingException e)
+            {
+                e.printStackTrace();
             }
         }
         public static void executeUpdate(){}
         public static void executeDelete(){}
 
-    }// calss
+    }
